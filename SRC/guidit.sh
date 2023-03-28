c77f0d82050f2908d6391502b7154ea5c6997a63
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-03-28 23:05:23 +0100

    Update and rename guidit.sh to uuidit.sh

--- SRC/guidit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 /opt/Go-go-go-GUID-it/SRC/guidit.py
-
-sleep 2s
-
-xdg-open /opt/Go-go-go-GUID-it/SRC/GUID.txt
---
+++ SRC/uuidit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 /opt/Lets-UUID-it/SRC/uuidit.py
+
+sleep 2s
+
+xdg-open /opt/Lets-UUID-it/SRC/UUID.txt
