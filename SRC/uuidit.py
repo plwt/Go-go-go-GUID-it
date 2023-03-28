cdb11a739871ee73e24ccf7f295288a75e6df3ec
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-03-28 23:04:11 +0100

    Update and rename guidit.py to uuidit.py

---
+++ SRC/uuidit.py
@@ -0,0 +1,22 @@
+import uuid
+
+def uuidit():
+    
+    # Create UUID
+    random_uuid = uuid.uuid4()
+
+    # Create text file for UUID
+    textfile=open('/opt/Lets-UUID-it/SRC/UUID.txt', 'w', encoding='utf-8')
+    textfile.write('UUID =  \n')
+    textfile.write('\n')
+    
+    # Enter UUID into text file and save file
+    for element in random_uuid:
+        textfile.write(element)
+    textfile.write('\n')
+    textfile.write('\n')
+
+    textfile.close()
+
+#run UUID generator 
+uuidit()
