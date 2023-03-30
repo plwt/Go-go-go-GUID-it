04599000baa4a44eedad229bffd6fa4155e2ac8b
Paul Wright <31939113+plwt@users.noreply.github.com>
2023-03-30 23:03:20 +0100

    Create uuidit.py

---
+++ SRC/uuidit.py
@@ -0,0 +1,27 @@
+import uuid
+import subprocess
+
+paths = {"openfile" = "/opt/Lets-UUID-it/SRC/UUID.txt"}
+
+def uuidit():
+    
+    # Create UUID
+    random_uuid = uuid.uuid4()
+
+    # Create text file for UUID
+    textfile=open('/opt/Lets-UUID-it/SRC/UUID.txt', 'w', encoding='utf-8')
+    textfile.write('UUID =  \n')
+    textfile.write('\n')
+    
+    # Enter UUID into text file and save file
+    for element in random_uuid:
+        textfile.write(element)
+    textfile.write('\n')
+    textfile.write('\n')
+
+    textfile.close()
+    
+    subprocess.Popen(paths["fileopen"])
+
+#run UUID generator 
+uuidit()
